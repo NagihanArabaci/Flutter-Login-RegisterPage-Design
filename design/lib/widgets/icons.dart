b4d0fb17413cc646568905cc694944dce5bcaaae
Nagihan <51118453+NagihanArabaci@users.noreply.github.com>
2022-04-19 14:33:01 +0300

    Delete design/lib/widgets directory

--- design/lib/widgets/icons.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Icons{
-static const IconData google=IconData(0xe900,fontFamily: 'Icons');
-static const IconData facebook=IconData(0xe901,fontFamily: 'Icons');
-static const IconData twitter=IconData(0xe902,fontFamily: 'Icons');
-}
